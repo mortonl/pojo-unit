af47e9e3b1ebc6b159e9f7e367dc70c279576f44
Luke Morton <luke.morton@hermes-europe.co.uk>
2016-11-29 09:37:07 +0000

    further attempt at fixing

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-if [ ! -z "$TRAVIS_TAG" ]
+if [[ ! -z "$TRAVIS_TAG" ]];
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
